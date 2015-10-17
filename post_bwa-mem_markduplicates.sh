f77159ddb781083d7e527bd3255c7e7bdf9e209f
Martin Bontrager <mbontrager@gmail.com>
2015-10-17 18:51:53 -0500

    Read coverage pipeline

---
+++ post_bwa-mem_markduplicates.sh
@@ -0,0 +1,51 @@
+﻿#!/bin/bash
+# Default parameters
+RMTMPFILES=true
+CALCCOV=true
+THREADS=1
+RNAME=$1
+QNAME="Eaff"
+CURDIR=`pwd`
+
+mkdir -p $RNAME
+cd $RNAME
+
+# Index sorted bam file
+samtools index ../${RNAME}_${QNAME}.sorted.bam
+
+# Mark duplicates and sort
+java -Djava.io.tmpdir=`pwd`/tmp -jar \
+    /home/lee/bioinformatics/picard-tools-1.135/picard.jar MarkDuplicates \
+    INPUT=../${RNAME}_${QNAME}.sorted.bam \
+    OUTPUT=${RNAME}_${QNAME}-smd.bam \
+    METRICS_FILE=${RNAME}_${QNAME}-smd.metrics \
+    AS=TRUE \
+    VALIDATION_STRINGENCY=LENIENT \
+    MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 \
+    REMOVE_DUPLICATES=TRUE
+    TMP_DIR=`pwd`/tmp
+samtools sort ${RNAME}_${QNAME}-smd.bam ${RNAME}_${QNAME}-smds
+samtools index ${RNAME}_${QNAME}-smds.bam
+
+# Determine Genome Coverage and mean coverage per contig
+if $CALCCOV; then
+    genomeCoverageBed -ibam ${RNAME}_${QNAME}-smds.bam > ${RNAME}_${QNAME}-smds.coverage
+    awk 'BEGIN {pc=""}
+    {
+        c=$1;
+        if (c == pc) {
+            cov=cov+$2*$5;
+        } else {
+            print pc,cov;
+            cov=$2*$5;
+        pc=c}
+    } END {print pc,cov}' ${RNAME}_${QNAME}-smds.coverage | tail -n +2 > ${RNAME}_${QNAME}-smds.coverage.percontig
+fi
+
+# Remove temp files
+if $RMTMPFILES; then
+    rm ../${RNAME}_${QNAME}.sorted.bam \
+       ${RNAME}_${QNAME}-smd.bam \
+fi
+
+cd $CURDIR
---
+++ scripts/bwa-stampy_markduplicates.sh
@@ -0,0 +1,45 @@
+﻿#!/bin/bash
+# Default parameters
+RMTMPFILES=true
+CALCCOV=true
+THREADS=1
+RNAME=$1
+CURDIR=`pwd`
+REF=$2
+
+mkdir -p $RNAME
+cd $RNAME
+
+# Sort bam file
+samtools sort ../${RNAME}.bam ../${RNAME}-s
+
+# Index sorted bam file
+samtools index ../${RNAME}-s.bam
+
+# Mark duplicates and sort
+java -Djava.io.tmpdir=`pwd`/tmp -jar \
+    /home/lee/bioinformatics/picard-tools-1.135/picard.jar MarkDuplicates \
+    INPUT=../${RNAME}-s.bam \
+    OUTPUT=${RNAME}-smd.bam \
+    METRICS_FILE=${RNAME}-smd.metrics \
+    AS=TRUE \
+    VALIDATION_STRINGENCY=LENIENT \
+    MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 \
+    REMOVE_DUPLICATES=TRUE
+    TMP_DIR=`pwd`/tmp
+samtools sort ${RNAME}-smd.bam ${RNAME}-smds
+samtools index ${RNAME}-smds.bam
+
+# Determine Genome Coverage and mean coverage per contig
+if $CALCCOV; then
+    genomeCoverageBed -ibam ${RNAME}-smds.bam > ${RNAME}-smds.coverage
+   # generate table with length and coverage stats per contig (From http://github.com/BinPro/CONCOCT)
+    python gen_contig_cov_per_bam_table.py --isbedfiles $REF ${RNAME}-smds.coverage > ${RNAME}-smds.coverage.percontig
+fi
+
+# Remove temp files
+if $RMTMPFILES; then
+    rm ../${RNAME}-s* ${RNAME}-smd.bam 
+fi
+
+cd $CURDIR
