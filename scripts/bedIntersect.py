2ae3d798ecdd6a4c43a09f75dba662c641a9479b
Martin Bontrager <mbontrager@gmail.com>
2016-09-22 10:25:39 -0500

    Change window size

--- scripts/bedIntersect.py
+++ scripts/bedIntersect.py
@@ -10,32 +10,35 @@ import getopt
 # Input - path to .out file
 #
 # Usage:
-# bedIntersect.py -i <path_to_file.out> -o <path_to_out.bed>
+# bedIntersect.py -w WINDOW_SIZE -i <path_to_file.out> -o <path_to_out.bed>
 #
 # Author: Martin Bontrager
 ############################################################
 
-
 def main():
+    window = 2500
+    
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hi:o:",
+        opts, args = getopt.getopt(sys.argv[1:], "hw:i:o:",
                                    ["input=", "output="])
     except getopt.GetoptError:
-        print('bedIntersect.py -i <path_to_file.out> -o <path_to_out.bed>')
+        print('bedIntersect.py -w WINDOW_SIZE -i <path_to_file.out> -o <path_to_out.bed>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('bedIntersect.py -i <path_to_file.out> -o <path_to_out.bed>')
+            print('bedIntersect.py -w WINDOW_SIZE -i <path_to_file.out> -o <path_to_out.bed>')
             sys.exit()
+        elif opt in ("-w", "--window"):
+            window = int(arg)
         elif opt in ("-i", "--input"):
             infile = arg
         elif opt in ("-o", "--output"):
             outfile = arg
 
-    create_bed(infile, outfile)
+    create_bed(infile, outfile, window)
 
 
-def create_bed(infile, outfile):
+def create_bed(infile, outfile, window):
     # Create a bed format file from the popoolation output file. Regions are
     # 5kb so I add and subtract 2500 from each position
     
@@ -45,8 +48,8 @@ def create_bed(infile, outfile):
         for row in reader:
             newrow = [None] * 3
             newrow[0] = row[0].strip('"')
-            newrow[2] = int(row[1]) + 2500
-            newrow[1] = int(row[1]) - 2500
+            newrow[2] = int(row[1]) + window
+            newrow[1] = int(row[1]) - window
             writer.writerow(newrow)
 
 if __name__ == '__main__':
